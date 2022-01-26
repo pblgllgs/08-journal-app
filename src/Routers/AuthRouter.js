4356e50ff2c786bbf008593ffa339639bfbe87e5
pblgllgs <pbl.gllgs@gmail.com>
2022-01-26 14:11:00 -0300

    Scss login styles

--- src/Routers/AuthRouter.js
+++ src/Routers/AuthRouter.js
@@ -5,16 +5,18 @@ import { RegisterScreen } from '../components/auth/RegisterScreen';
 
 export const AuthRouter = () => {
     return (
-        <div>
-            <Switch>
-                <Route exact path="/auth/login">
-                    <LoginScreen />
-                </Route>
-                <Route exact path="/auth/register">
-                    <RegisterScreen />
-                </Route>
-                <Redirect to="/auth/login" />
-            </Switch>
+        <div className="auth__main">
+            <div className='auth__box-container'>
+                <Switch>
+                    <Route exact path="/auth/login">
+                        <LoginScreen />
+                    </Route>
+                    <Route exact path="/auth/register">
+                        <RegisterScreen />
+                    </Route>
+                    <Redirect to="/auth/login" />
+                </Switch>
+            </div>
         </div>
     );
 };
