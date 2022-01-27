02af23a825878274562f5f3bed81167b02e314f2
pblgllgs <pbl.gllgs@gmail.com>
2022-01-27 09:49:32 -0300

    note screen sass scss

--- src/components/notes/NoteScreen.js
+++ src/components/notes/NoteScreen.js
@@ -2,7 +2,24 @@ import React from 'react';
 import { NotesAppBar } from './NotesAppBar';
 
 export const NoteScreen = () => {
-    return <div className="notes__main-content">
-        <NotesAppBar />
-    </div>;
+    return (
+        <div className="notes__main-content">
+            <NotesAppBar />
+            <div className="notes__content">
+                <input
+                    type="text"
+                    placeholder="Some  awesome title"
+                    className="notes__title-input"
+                    autoComplete='off'
+                />
+                <textarea
+                    placeholder="What happened today"
+                    className="notes__textarea"
+                ></textarea>
+                <div className="notes__images">
+                    <img src="https://i.picsum.photos/id/1072/200/300.jpg?hmac=uzq3N0ox40X06q0Ql4mCdgMwHc13gIa0QAuu_6Zp6lQ" alt='imagen' />
+                </div>
+            </div>
+        </div>
+    );
 };
