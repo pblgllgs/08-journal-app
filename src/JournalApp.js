20763b85abfe8877e13c70d4ae2918a1268d3e9d
pblgllgs <pbl.gllgs@gmail.com>
2022-01-28 12:17:29 -0300

    install react-redux redux, config, store, authReducer

--- src/JournalApp.js
+++ src/JournalApp.js
@@ -1,10 +1,14 @@
 import React from 'react';
+
+import { Provider } from 'react-redux';
+import { store } from './store/store';
+
 import { AppRouter } from './Routers/AppRouter';
 
 export const JournalApp = () => {
     return (
-        <div>
+        <Provider store={store}>
             <AppRouter />
-        </div>
+        </Provider>
     );
 };
