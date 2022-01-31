9911ec82c0b96407bc4369dd3b9a4f5eb887b57e
pblgllgs <pbl.gllgs@gmail.com>
2022-01-31 12:52:42 -0300

    ui store, types

--- src/components/auth/RegisterScreen.js
+++ src/components/auth/RegisterScreen.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { useForm } from '../../hooks/useForm';
 import validator from 'validator';
+import { useDispatch, useSelector } from 'react-redux';
+
+import { useForm } from '../../hooks/useForm';
+import { removeError, setError } from '../../actions/ui';
+import { startRegisterWithEmailPasswordName } from '../../actions/auth';
 
 export const RegisterScreen = () => {
+    const dispatch = useDispatch();
+    const { msgError } = useSelector((state) => state.ui);
+
     //valor inicial
     const initialForm = {
         name: 'Pablo',
@@ -21,22 +28,26 @@ export const RegisterScreen = () => {
     const handleRegister = (e) => {
         e.preventDefault();
         if (isFormValid()) {
-            console.log('form success');
+            dispatch(startRegisterWithEmailPasswordName(email, password,name));
         }
     };
 
     const isFormValid = () => {
         if (name.trim().length === 0) {
-            console.log('name is required');
+            const msg = 'name is required';
+            dispatch(setError(msg));
             return false;
         } else if (!validator.isEmail(email)) {
-            console.log('email is required');
+            const msg = 'email is not valid';
+            dispatch(setError(msg));
             return false;
-        }else if(password !== password2 || password.length < 5){
-            console.log('Password shoud be at least 6 caracters and mach each other');
+        } else if (password !== password2 || password.length < 5) {
+            const msg =
+                'Password shoud be at least 6 caracters and mach each other';
+            dispatch(setError(msg));
             return false;
         }
-
+        dispatch(removeError());
         return true;
     };
 
@@ -44,7 +55,9 @@ export const RegisterScreen = () => {
         <>
             <h3 className="auth__title">Register</h3>
             <form onSubmit={handleRegister}>
-                <div className="auth__alert-error">Error</div>
+                {msgError !== null && (
+                    <div className="auth__alert-error">{msgError}</div>
+                )}
                 <input
                     className="auth__input"
                     type="text"
--- src/types/types.js
+++ src/types/types.js
@@ -2,5 +2,8 @@
 
 export const types = {
     login: '[Auth] Login',
-    logout: '[Auth] Logout'
+    logout: '[Auth] Logout',
+
+    uiSetError: '[UI] set error',
+    uiRemoveError: '[UI] remove error'
 }
