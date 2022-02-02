6c064c965aeb370b8597abe756d6f68bd57877a6
pblgllgs <pbl.gllgs@gmail.com>
2022-02-02 14:30:31 -0300

    show data from prps

--- src/components/journal/JournalEntry.js
+++ src/components/journal/JournalEntry.js
@@ -1,24 +1,32 @@
 import React from 'react';
+import moment from 'moment';
 
-export const JournalEntry = () => {
+export const JournalEntry = ({id, date, title, body, url}) => {
+
+    const notaDate = moment(date);
+
     return (
         <div className="journal__entry pointer">
-            <div
-                className="journal__entry-picture"
-                style={{
-                    backgroundSize: 'cover',
-                    backgroundImage:
-                        'url(https://desafiolatam.com/assets/cursos/react/icono-react-8e9b14b4495e5a21ccd56ea61f95a7fc30fc5eb8ee92e44fefd1277d62a93296.png)',
-                }}
-            ></div>
+            {url && (
+                <div
+                    className="journal__entry-picture"
+                    style={{
+                        backgroundSize: 'cover',
+                        backgroundImage: `url(${url})`,
+                    }}
+                ></div>
+            )}
+
             <div className="journal__entry-body">
-                <p className="journal__entry-title">Un nuevo día...</p>
-                <p className="journal__entry-content">Cupidatat aliquip consectetur eu excepteur est elit aliquip fugiat cupidatat et aliqua nostrud.</p>
+                <p className="journal__entry-title">{title}</p>
+                <p className="journal__entry-content">{body}</p>
             </div>
 
-            <div className='journal__entry-date-box'>
-              <span className='journal__entry-span'>Monday</span>
-              <h4>28</h4>
+            <div className="journal__entry-date-box">
+                <span className="journal__entry-span">
+                    {notaDate.format('dddd')}
+                </span>
+                <h4>{notaDate.format('Do')}</h4>
             </div>
         </div>
     );
