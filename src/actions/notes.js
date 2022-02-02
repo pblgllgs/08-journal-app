14a811dd764eb962d1de8236722f4e411c566347
pblgllgs <pbl.gllgs@gmail.com>
2022-02-02 13:25:03 -0300

    action notes

---
+++ src/actions/notes.js
@@ -0,0 +1,28 @@
+import { addDoc, collection } from 'firebase/firestore';
+import { db } from '../firebase/firebase-config';
+import { types } from '../types/types';
+
+export const startNewNotes = () => {
+    return async (dispatch, getState) => {
+        const { uid } = getState().auth;
+        const newNote = {
+            title: '',
+            body: '',
+            date: new Date().getTime(),
+        };
+        const doc = await addDoc(
+            collection(db, `${uid}/journal/notes`),
+            newNote
+        );
+
+        dispatch(activeNote(doc.id, newNote));
+    };
+};
+
+export const activeNote = (id, note) => ({
+    type: types.notesActive,
+    payload: {
+        id,
+        ...note,
+    },
+});
