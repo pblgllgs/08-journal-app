a81fea044251d003b7e40645f8d14941afb37888
pblgllgs <pbl.gllgs@gmail.com>
2022-02-02 13:24:35 -0300

    add method handle, name

--- src/components/journal/Sidebar.js
+++ src/components/journal/Sidebar.js
@@ -1,14 +1,21 @@
 import React from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { startLogout } from '../../actions/auth';
+import { startNewNotes } from '../../actions/notes';
 import { JournalEntries } from './JournalEntries';
 
 export const Sidebar = () => {
 
     const dispatch  = useDispatch();
 
+    const {name}  = useSelector(state => state.auth);
+
     const handleLogout= () => {
         dispatch(startLogout());
+    }
+
+    const HandleAddNew = () => {
+        dispatch(startNewNotes());
     }
 
     return (
@@ -16,13 +23,15 @@ export const Sidebar = () => {
             <div className="journal__sidebar-navbar">
                 <h3 className="mt-5">
                     <i className="far fa-moon"></i>
-                    <span> Pablo</span>
+                    <span> {name}</span>
                 </h3>
 
-                <button className="btn" onClick={handleLogout} >Logout</button>
+                <button className="btn" onClick={handleLogout}>
+                    Logout
+                </button>
             </div>
 
-            <div className="journal__new-entry">
+            <div className="journal__new-entry" onClick={HandleAddNew}>
                 <i className="far fa-calendar-plus fa-5x"></i>
                 <p className="mt-5">New Entry</p>
             </div>
