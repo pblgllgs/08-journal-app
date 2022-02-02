0944e265d2623519d55e9cbc820436269cf143ff
pblgllgs <pbl.gllgs@gmail.com>
2022-02-02 13:23:07 -0300

    add notesReducer

--- src/store/store.js
+++ src/store/store.js
@@ -2,6 +2,7 @@ import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 
 import { authReducer } from '../reducers/authReducer';
+import { notesReducer } from '../reducers/notesReducer';
 import { uiReducer } from '../reducers/uiReducer';
 
 const composeEnhancers =
@@ -11,7 +12,8 @@ const composeEnhancers =
 
 const reducers = combineReducers({
     auth: authReducer,
-    ui: uiReducer
+    ui: uiReducer,
+    notes: notesReducer
 });
 
 export const store = createStore(
