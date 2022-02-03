6c8f9e51b7b5ee132d9040da93298f03a9a5c1db
pblgllgs <pbl.gllgs@gmail.com>
2022-02-03 09:55:43 -0300

    handle save

--- src/components/notes/NotesAppBar.js
+++ src/components/notes/NotesAppBar.js
@@ -1,6 +1,17 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { startSaveNote } from '../../actions/notes';
 
 export const NotesAppBar = () => {
+    const dispatch  = useDispatch();
+
+    const {active}  = useSelector(state => state.notes);
+
+    const handleSave = () => {
+        console.log(active);
+        dispatch(startSaveNote(active));
+    }
+
     return (
         <div className="notes__appbar">
             <span>28 de agosto 2020</span>
@@ -8,7 +19,7 @@ export const NotesAppBar = () => {
                 <button className="btn">Picture</button>
             </div>
             <div>
-                <button className="btn">Save</button>
+                <button className="btn" onClick={handleSave}>Save</button>
             </div>
         </div>
     );
