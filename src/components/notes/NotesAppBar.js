6ff0f7f23a3c64d7bc5d25bee16b682fd091c605
pblgllgs <pbl.gllgs@gmail.com>
2022-02-14 10:48:00 -0300

    handlefile, handlepicture

--- src/components/notes/NotesAppBar.js
+++ src/components/notes/NotesAppBar.js
@@ -1,25 +1,48 @@
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { startSaveNote } from '../../actions/notes';
+import { startSaveNote, startUploading } from '../../actions/notes';
 
 export const NotesAppBar = () => {
-    const dispatch  = useDispatch();
+    const dispatch = useDispatch();
 
-    const {active}  = useSelector(state => state.notes);
+    const { active } = useSelector((state) => state.notes);
 
     const handleSave = () => {
         console.log(active);
         dispatch(startSaveNote(active));
-    }
+    };
+
+    const handlePicture = () => {
+        document.querySelector('#fileSelector').click();
+    };
+
+    const handleFileChange = (e) => {
+        const file = e.target.files[0];
+        if (file) {
+            dispatch(startUploading(file));
+        }
+    };
 
     return (
         <div className="notes__appbar">
             <span>28 de agosto 2020</span>
+
+            <input
+                type="file"
+                style={{ display: 'none' }}
+                onChange={handleFileChange}
+                id="fileSelector"
+            />
+
             <div>
-                <button className="btn">Picture</button>
+                <button className="btn" onClick={handlePicture}>
+                    Picture
+                </button>
             </div>
             <div>
-                <button className="btn" onClick={handleSave}>Save</button>
+                <button className="btn" onClick={handleSave}>
+                    Save
+                </button>
             </div>
         </div>
     );
