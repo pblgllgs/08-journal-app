f3fefd983e4b657d951a879a35a1e61d618fefde
pblgllgs <pbl.gllgs@gmail.com>
2022-02-14 12:19:55 -0300

    add actions, delete, addnew, swal msgs

--- src/actions/notes.js
+++ src/actions/notes.js
@@ -1,4 +1,10 @@
-import { addDoc, collection, doc, updateDoc } from 'firebase/firestore';
+import {
+    addDoc,
+    collection,
+    deleteDoc,
+    doc,
+    updateDoc,
+} from 'firebase/firestore';
 import Swal from 'sweetalert2';
 import { db } from '../firebase/firebase-config';
 import { fileUpload } from '../helpers/fileUpload';
@@ -19,12 +25,20 @@ export const startNewNotes = () => {
         );
 
         dispatch(activeNote(doc.id, newNote));
-        dispatch(startLoadingNotes(uid));
+        dispatch(addNewNote(doc.id, newNote));
     };
 };
 
 export const activeNote = (id, note) => ({
     type: types.notesActive,
+    payload: {
+        id,
+        ...note,
+    },
+});
+
+export const addNewNote = (id, note) => ({
+    type: types.notesAddNew,
     payload: {
         id,
         ...note,
@@ -78,7 +92,37 @@ export const startUploading = (file) => {
     return async (dispatch, getState) => {
         const { active: activeNote } = getState().notes;
 
+        Swal.fire({
+            title: 'Uploading...',
+            text: 'Please await...',
+            allowOutsideClick: false,
+        });
+        Swal.showLoading();
+
         const fileUrl = await fileUpload(file);
-        console.log(fileUrl);
+
+        activeNote.url = fileUrl;
+
+        dispatch(startSaveNote(activeNote));
+
+        Swal.close();
     };
 };
+
+export const startDeleting = (id) => {
+    return async (dispatch, getState) => {
+        const uid = getState().auth.uid;
+        await deleteDoc(doc(db, `${uid}/journal/notes`, `${id}`));
+        dispatch(deleteNote(id));
+        Swal.fire('Eliminada', 'Nota eliminada con exito', 'success');
+    };
+};
+
+export const deleteNote = (id) => ({
+    type: types.notesDelete,
+    payload: id,
+});
+
+export const noteLogout = () => ({
+    type: types.notesLogoutCleaning,
+});
