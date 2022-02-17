149cb1bec6c7d763d72a8205d72e1e6e1204ed64
pblgllgs <pbl.gllgs@gmail.com>
2022-02-17 12:13:15 -0300

    refactor env

--- src/firebase/firebase-config.js
+++ src/firebase/firebase-config.js
@@ -7,17 +7,33 @@ import { GoogleAuthProvider } from 'firebase/auth';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: 'AIzaSyCHNU7JjZyj7ppRJjVp9I_FAYZB0JibCkU',
-    authDomain: 'journal-app-2b360.firebaseapp.com',
-    projectId: 'journal-app-2b360',
-    storageBucket: 'journal-app-2b360.appspot.com',
-    messagingSenderId: '1060476625001',
-    appId: '1:1060476625001:web:b1f4260476f62fa57e42f8',
+    apiKey: process.env.REACT_APP_APIKEY,
+    authDomain: process.env.REACT_APP_AUTHDOMAIN,
+    projectId: process.env.REACT_APP_PROJECTID,
+    storageBucket: process.env.REACT_APP_STORAGEBUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID,
+    appId: process.env.REACT_APP_APPID,
 };
+
+// const firebaseConfigTesting = {
+//     apiKey: 'AIzaSyDSFXG4SLFINILX9Ls-ajCepvYGe6q5BdY',
+//     authDomain: 'journal-test-461ca.firebaseapp.com',
+//     projectId: 'journal-test-461ca',
+//     storageBucket: 'journal-test-461ca.appspot.com',
+//     messagingSenderId: '1036753957074',
+//     appId: '1:1036753957074:web:999af218ad96e302b0bd0b',
+// };
+
+// if(process.env.NODE_ENV === 'test'){
+//     const app = initializeApp(firebaseConfigTesting);
+// }else{
+//     const app = initializeApp(firebaseConfig);
+// }
+
+const app = initializeApp(firebaseConfig);
 
 // Initialize Firebase
 // eslint-disable-next-line no-unused-vars
-const app = initializeApp(firebaseConfig);
 
 const db = getFirestore()
 const googleAuthProvider = new GoogleAuthProvider();
