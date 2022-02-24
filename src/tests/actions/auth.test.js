0293d1188cf507ccd76c1750a9fb3c3b513ec0e7
pblgllgs <pbl.gllgs@gmail.com>
2022-02-24 10:33:09 -0300

    test auth

--- src/tests/actions/auth.test.js
+++ src/tests/actions/auth.test.js
@@ -1,28 +1,28 @@
 import thunk from 'redux-thunk';
 import { types } from '../../types/types';
 import configureStore from 'redux-mock-store'; //ES6 modules
-import { login, logout } from '../../actions/auth';
+import {
+    login,
+    logout,
+    startLoginEmailPassword,
+    startLogout,
+} from '../../actions/auth';
+import { ActionCodeOperation } from 'firebase/auth';
 
 const middlewares = [thunk];
 const mockStore = configureStore(middlewares);
 
-const initState = {
-    auth: {
-        uid: '28jgTFniAeTMYs7ENdLs',
-        displayName: 'pablo',
-    },
-};
+const initState = {};
 
 let store = mockStore(initState);
 
 describe('Pruebas en auth/actions', () => {
-
     beforeEach(() => {
         store = mockStore(initState);
     });
 
     test('login debe de crear la accion respectiva', () => {
-        store.dispatch(login(initState.auth.uid,initState.auth.displayName));
+        store.dispatch(login('28jgTFniAeTMYs7ENdLs', 'pablo'));
 
         const dataLogin = {
             type: types.login,
@@ -45,4 +45,45 @@ describe('Pruebas en auth/actions', () => {
         const actions = store.getActions();
         expect(actions[0]).toEqual(dataLogout);
     });
+
+    test('debe de realizar el startLogout', async () => {
+        await store.dispatch(startLogout());
+
+        const actions = store.getActions();
+        expect(actions[0]).toEqual({
+            type: types.logout,
+        });
+        expect(actions[1]).toEqual({
+            type: types.notesLogoutCleaning,
+        });
+    });
+
+    test('debe de iniciar el startLoginEmailPassword', async () => {
+        const email = 'pbl.gllgs@email.com';
+        const password = '123123';
+
+        await store.dispatch(startLoginEmailPassword(email, password));
+
+        const actions = store.getActions();
+
+        console.log(actions)
+
+        expect(actions[0]).toEqual({
+            type: types.uiStartLoading,
+        });
+
+        expect(actions[1]).toEqual({
+            type: types.login,
+            payload: {
+                uid: 'p5z8LgJuShZNz0Xm5pwbNgdst7n1',
+                displayName: null
+            },
+        });
+
+        expect(actions[2]).toEqual({
+            type: types.uiFinishLoading
+        })
+    });
+
+    
 });
