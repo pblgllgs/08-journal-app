bb972d0f2d5dcbbd6da60d9f73a933b92a82b79e
pblgllgs <pbl.gllgs@gmail.com>
2022-02-25 11:37:34 -0300

    test register

---
+++ src/tests/components/auth/RegisterScreen.test.js
@@ -0,0 +1,87 @@
+import { mount } from 'enzyme';
+import { Provider } from 'react-redux';
+import { MemoryRouter } from 'react-router-dom';
+
+import configureStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
+
+import '@testing-library/jest-dom';
+import { RegisterScreen } from '../../../components/auth/RegisterScreen';
+
+const middlewares = [thunk];
+const mockStore = configureStore(middlewares);
+
+const initState = {
+    auth: {},
+    ui: {
+        loading: false,
+        msgError: null,
+    },
+};
+
+let store = mockStore(initState);
+
+const wrapper = mount(
+    <Provider store={store}>
+        <MemoryRouter>
+            <RegisterScreen />
+        </MemoryRouter>
+    </Provider>
+);
+
+describe('Pruebas en <RegisterScreen />', () => {
+    beforeEach(() => {
+        store = mockStore(initState);
+        jest.clearAllMocks();
+    });
+
+    test('debe de mostrarse correctamente', () => {
+        expect(wrapper).toMatchSnapshot();
+    });
+
+    // test('debe de hacer el dispatch de la accion respectiva', () => {
+    //     const emailField = wrapper.find('input[name="email"]');
+    //     emailField.simulate('change', {
+    //         target: {
+    //             value: '',
+    //             name: 'email',
+    //         },
+    //     });
+
+    //     wrapper.find('form').simulate('submit', {
+    //         preventDefault() {},
+    //     });
+
+    //     const actions = store.getActions();
+    //     console.log(actions)
+    //     expect(actions[0]).toEqual({
+    //         type: types.uiSetError,
+    //         payload: 'email is not valid',
+    //     });
+    // });
+
+    test('debe de mostrar el error', () => {
+        const initState = {
+            auth: {},
+            ui: {
+                loading: false,
+                msgError: 'Email no es correcto',
+            },
+        };
+
+        let store = mockStore(initState);
+
+        const wrapper = mount(
+            <Provider store={store}>
+                <MemoryRouter>
+                    <RegisterScreen />
+                </MemoryRouter>
+            </Provider>
+        );
+
+        expect(wrapper.find('.auth__alert-error').exists()).toBe(true);
+        expect(wrapper.find('.auth__alert-error').text().trim()).toBe(
+            initState.ui.msgError
+        );
+    });
+});
